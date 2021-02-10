01440086caa95039e1a28b1705d41f2a62cf0335
Daniel Moura <daniel.mlds@gmail.com>
2021-02-10 19:36:29 -0300

    Correção de erros

--- classes/anuncios.class.php
+++ classes/anuncios.class.php
@@ -5,22 +5,21 @@
             global $pdo;
             $array = array();
 
-        /*    
+           
             $sql = $pdo->prepare("SELECT
             *,
-            (SELECT anuncios_imagens.url FROM anuncios_images WHERE anuncios_images.
-            id_anuncio = anuncio.id limit 1) as url 
+            (SELECT anuncios_imagens.url FROM anuncios_imagens WHERE anuncios_imagens.
+            id_anuncio = anuncios.id limit 1) as url 
             FROM anuncios 
-            WHERE id_usuario = :id_isuario");
-        */
+            WHERE id_usuario = :id_usuario");
+        
 
-            $sql->bindValue(":id_isuario", $_SESSION['cLogin']);
+            $sql->bindValue(":id_usuario", $_SESSION['cLogin']);
             $sql->execute();
 
             if($sql->rowCount() > 0) {
                 $array = $sql->fetchAll();
             }
-
             return $array;
         }
 
